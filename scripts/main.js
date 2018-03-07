b19a9f8c930b41d21e48f8380cc475ba1e4c9cd5
Ashley Parker <aparker7698@att.net>
2018-03-07 18:11:33 -0500

    Added Login Button and Menu

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+//introduction image insertion
+var introSetup = function() {
+    var intro = document.querySelector('.introduction');
+    var introImage= document.createElement('img');
+    introImage.src = 'https://images.pexels.com/photos/325521/pexels-photo-325521.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
+    intro.appendChild(introImage);
+}
+introSetup();
+
+var photoGallery = document.querySelector('.photos');
+var imgGallery = [ 
+    {
+        link: "images/pg1.jpg"
+    },
+    {
+        link: "images/pg2.jpg"
+    },
+    {
+        link: "images/pg3.jpg"
+    },
+    {
+        link: "images/pg4.jpg"
+    }
+];
+var imageDisplay = function(gallery) {
+    gallery.forEach(function(image) {
+        var imageTag = document.createElement('img');
+        imageTag.setAttribute('src', image.link);
+        photoGallery.appendChild(imageTag);
+    });
+};
+
+imageDisplay(imgGallery);
+
+
+
+document.addEventListener('click', function(e) {
+    var loginButton = document.querySelector('#login-trigger')
+    var loginMenu = document.querySelector('.login-content');
+    var loginForm = document.querySelector('.login-form')
+    console.log(loginForm)
+    console.log(e.target.parentElement)
+    if (e.target === loginButton) {
+        loginMenu.classList.remove('inactive');
+    } else if (e.target.parentElement === loginForm) {
+        loginMenu.classList.remove('inactive');
+    } else {
+        loginMenu.classList.add('inactive');
+    }
+});
+
+
