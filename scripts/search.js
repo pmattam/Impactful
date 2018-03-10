e57efc19579397efeb418331fdd2319f78dbe61d
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-03-10 12:39:40 -0500

    Added styling to the table for the search results

--- scripts/search.js
+++ scripts/search.js
@@ -35,25 +35,30 @@ var charityDisplay = function(charityList) {
 var addCharityListToTable = function(charityList) {
     var searchDiv = document.querySelector('.search-results');
     var table = document.createElement('table');
-    var header = table.createTHead();
-    var hrow = header.insertRow();
-    var hNameCell = hrow.insertCell(0);
-    hNameCell.textContent = 'ORGANIZATION NAME';
-    var hAddressCell = hrow.insertCell(1);
-    hAddressCell.textContent = 'ADDRESS';
-    var hRatingCell = hrow.insertCell(2);
-    hRatingCell.textContent = 'RATING';
+    var headerRow = table.insertRow(-1);
+    var headerNameCell = document.createElement('TH');
+    headerNameCell.textContent = 'ORGANIZATION NAME';
+    headerRow.appendChild(headerNameCell);
+
+    var headerAddressCell = document.createElement('TH');
+    headerAddressCell.textContent = 'ADDRESS';
+    headerRow.appendChild(headerAddressCell);
+
+    var headerRatingCell = document.createElement('TH');
+    headerRatingCell.textContent = 'RATING';
+    headerRow.appendChild(headerRatingCell);
     searchDiv.appendChild(table);
+    
     charityList.forEach(function(charity) {
-    var img = document.createElement('img');
-    var row = table.insertRow();
-    var nameCell = row.insertCell(0);
-    nameCell.textContent = charity.Name;
-    var addressCell = row.insertCell(1);
-    addressCell.textContent = charity.Address.streetAddress1 + ", " + charity.Address.city + ", " + charity.Address.stateOrProvince + ", " + charity.Address.postalCode;
-    var ratingCell = row.insertCell(2);
-    img.setAttribute('src', charity.Rating.ratingImage.large);
-    ratingCell.appendChild(img);
+        var img = document.createElement('img');
+        var row = table.insertRow();
+        var nameCell = row.insertCell(0);
+        nameCell.textContent = charity.Name;
+        var addressCell = row.insertCell(1);
+        addressCell.textContent = charity.Address.streetAddress1 + ", " + charity.Address.city + ", " + charity.Address.stateOrProvince + ", " + charity.Address.postalCode;
+        var ratingCell = row.insertCell(2);
+        img.setAttribute('src', charity.Rating.ratingImage.large);
+        ratingCell.appendChild(img);
     });
     searchDiv.appendChild(table);
 }
